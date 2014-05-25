20e829218f7a032eda4bb5bbb701a2f2aa0a7677
sbrown345 <simonbrown33@gmail.com>
2014-05-25 19:40:31 +0100

    rename, move doomdll stuff

--- src/doom3_emscripten/js_main.cpp
+++ src/doom3_emscripten/js_main.cpp
@@ -11,17 +11,17 @@
 
 int main(int argc, const char **argv) {
 	printf("hi\n");
-//	////Posix_EarlyInit( );
-//
-//	if ( argc > 1 ) {
-//		common->Init( argc-1, &argv[1], NULL );
-//	} else {
-//		common->Init( 0, NULL, NULL );
-//	}
-//
-//	////Posix_LateInit( );
-//
-//	while (1) {
-//		common->Frame();
-//	}
+	////Posix_EarlyInit( );
+
+	//if ( argc > 1 ) {
+	//	common->Init( argc-1, &argv[1], NULL );
+	//} else {
+	//	common->Init( 0, NULL, NULL );
+	//}
+
+	////Posix_LateInit( );
+
+	//while (1) {
+	//	common->Frame();
+	//}
 }
